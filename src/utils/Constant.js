4f54cedbb1692f0becaf76270a5953b1929cbf8e
sachinchauhan010 <139807470+sachinchauhan010@users.noreply.github.com>
2024-02-21 19:39:29 +0530

    Grub Express Code

---
+++ src/utils/Constant.js
@@ -0,0 +1,2 @@
+export const API_URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7322603&lng=83.4332587&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const IMG_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
